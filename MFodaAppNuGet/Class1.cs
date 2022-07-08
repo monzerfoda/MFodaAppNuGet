2d0908db87ddfbfd381f11b3c1c3d8767962094e
monzerfoda <monzerfoda@gmail.com>
2022-07-08 03:02:51 +0200

    Add project files.

---
+++ MFodaAppNuGet/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFodaAppNuGet
+{
+    public class Class1
+    {
+        public void Display(string input)
+        {
+            Console.WriteLine($"MFoda Tech Co. {input}");
+        } 
+
+    }
+}
